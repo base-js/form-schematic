08ee96c4224754eddbee7ddd9c55b4c8b076eafb
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2024-03-19 14:20:06 +0700

    update version

--- src/configs/parser.ts
+++ src/configs/parser.ts
@@ -3,25 +3,6 @@ import { createParser } from "@adifkz/exp-p";
 const parser = createParser({});
 
 parser.setFunctions({
-  is_email: (value: any) => {
-    try {
-      return !!value?.match(
-        /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g,
-      );
-    } catch (error) {
-      return false;
-    }
-  },
-  is_html_empty: (value: any) => {
-    try {
-      if (!value) return true;
-      if (value === "<div></div>") return true;
-      if (value === "<span></span>") return true;
-      return false;
-    } catch (error) {
-      return false;
-    }
-  },
 });
 
 export {
